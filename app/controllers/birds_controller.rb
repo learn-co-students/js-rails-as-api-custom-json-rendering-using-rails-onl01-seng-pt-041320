4407f4764aa8767556b218a72f39708eff11b4ad
Paul_Beckwith <303dog@gmail.com>
2020-11-12 14:31:49 -0700

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,20 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
+    #OR by using except: will do the same output...
+    #render json: birds, except: [:created_at, :updated_at]
   end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird 
+      render json: bird.slice(:id, :name, :species)  
+      #OR is the same as..
+      #render json: {id: bird.id, name: bird.name, species: bird.species}
+    else
+      render json: {message: 'Bird not found'}
+    end 
+  end
+  
 end
